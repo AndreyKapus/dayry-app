6df57b1db420d9e6f0913e27d7cb3d7e764e5b5b
AndreyKapus <kapustnikov@ukr.net>
2023-09-20 15:15:24 +0300

    delete note

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,16 +2,24 @@ import { useNotes } from "../store/store";
 
 const Notes = () => {
   const myNotes = useNotes((state) => state.notes);
-  console.log(myNotes);
+
+  const deleteNote = useNotes((state) => state.removeNote);
+
+  const handleDelete = (id) => {
+    deleteNote(id);
+  };
 
   return (
     <div>
       <ul>
         {myNotes &&
-          myNotes.map((note, index) => {
+          myNotes.map((note) => {
             return (
-              <li key={index}>
+              <li key={note.id}>
                 <p>{note.text}</p>
+                <button type="button" onClick={() => handleDelete(note.id)}>
+                  delete
+                </button>
               </li>
             );
           })}
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
+import { v4 as uuidv4 } from "uuid";
 
 export const useNotes = create(
   persist(
@@ -9,13 +10,19 @@ export const useNotes = create(
       setNote: (note) =>
         set((state) => {
           const newNote = {
+            id: uuidv4(),
             text: note,
           };
           return {
             notes: [...state.notes, newNote],
           };
         }),
-      //   removeNotes: () => set({ bears: 0 }),
+      removeNote: (noteId) =>
+        set((state) => {
+          return {
+            notes: state.notes.filter((note) => note.id !== noteId),
+          };
+        }),
     }),
     { name: "events-storage", storage: createJSONStorage(() => localStorage) }
   )
